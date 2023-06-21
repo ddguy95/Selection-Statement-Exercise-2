8846cef5cb9c817eb132b2050f5193c4576696ac
derrickdrye <dryd95@hotmail.com>
2023-06-21 15:10:52 -0500

    completion of exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Switch-Case
+
+            Console.WriteLine("What is your favorite subject in school?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I hate math!");
+                    break;
+                case "science":
+                    Console.WriteLine("I love science.");
+                    break;
+                case "english":
+                    Console.WriteLine("I like english too!");
+                    break;
+                case "social studies":
+                    Console.WriteLine("I don't care for social studies too much.");
+                    break;
+                case "computers":
+                    Console.WriteLine("That is the best subject - computer class!");
+                    break;
+                default:
+                    Console.WriteLine("I cannot remember if I took that subject.");
+                    break;
+
+            }
+
+
         }
     }
 }
